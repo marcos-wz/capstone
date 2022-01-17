ffe59a95e3a2bae16891da4127b09765bc62494e
Marcos Jauregui <it.marcosj@gmail.com>
2022-01-16 21:27:24 -0600

    fruit service mock

---
+++ services/fruit_service_mock.go
@@ -0,0 +1,18 @@
+package services
+
+// import "github.com/marcos-wz/capstone/models"
+
+// type mockFruitStore struct {
+// 	mock.Mock
+// }
+
+// func (o mockFruitStore) CreateFruit(obj *models.Fruit) error {
+// 	args := o.Called()
+// 	return args.Error(0)
+// }
+
+// func (o mockFruitStore) ListFruit(filter store.FruitFilter) *models.Fruits {
+// 	args := o.Called()
+
+// 	return args.Get(0).(*models.Fruits)
+// }
