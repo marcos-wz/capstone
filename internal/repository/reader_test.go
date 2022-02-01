5c021c9435e730ab79dced3bfb82f67f4e8e5824
Marcos Jauregui <it.marcosj@gmail.com>
2022-02-01 12:34:47 -0600

    reader units test

---
+++ internal/repository/reader_test.go
@@ -0,0 +1,76 @@
+package repository
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/marcos-wz/capstone/internal/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReader_ReadFruits(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		filePath string
+		response *entity.Fruits
+		err      string
+	}{
+		{
+			"Should return valid fruit list",
+			"../../data/fruits-test-ok.csv",
+			&entity.Fruits{},
+			"<nil>",
+		},
+		{
+			"Should return error `no such file or directory`",
+			"",
+			nil,
+			"open : no such file or directory",
+		},
+		{
+			"Should return valid fruit list, with parser error",
+			"../../data/fruits-test-error.csv",
+			&entity.Fruits{},
+			"parser error",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			repo := NewReader(tc.filePath)
+			fruits, err := repo.ReadFruits()
+			assert.Equal(t, tc.err, fmt.Sprintf("%v", err))
+			if err == nil {
+				t.Log("Total fruits:", len(*fruits))
+				for _, f := range *fruits {
+					t.Logf("%+v", f)
+				}
+			}
+		})
+	}
+}
+
+func TestReader_ParseFruit(t *testing.T) {
+	var testCases = []struct {
+		name        string
+		recordParam []string
+		reponse     *entity.Fruit
+		reponseErrs []error
+	}{
+		{
+			"success, no errors",
+			[]string{"1", "test", "test description"},
+			&entity.Fruit{},
+			[]error{},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := &reader{}
+			fruit, errs := r.parseFruitRecord(tc.recordParam)
+			t.Log("ERRORS:", errs)
+			// assert.Equal(t, tc.reponseErrs, errs)
+			// assert.NotEmpty(t, tc.reponse, fruit)
+			t.Log("ParsedFruit:", fruit)
+		})
+	}
+}
