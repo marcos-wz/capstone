9c82be69337f4a419c152f6b82257b61e666b164
Marcos Jauregui <it.marcosj@gmail.com>
2022-02-03 13:47:56 -0600

    return error response as string

--- internal/controller/filter.go
+++ internal/controller/filter.go
@@ -32,56 +32,36 @@ func NewFilterController(svc filterSvc) iFilterController {
 // IMPLEMENTATION ********************************
 
 func (fc *filterController) GetFruitsFilter(c echo.Context) error {
-	// Input params validation
-	p := &getFruitsFilterParams{
+	// Input Validation
+	p := &entity.FruitsFilterParams{
 		Filter: c.Param("filter"),
 		Value:  c.Param("value"),
 	}
-	// NOTE: How to bind by PATH
-	// if err := c.Bind(p); err != nil {
-	// if err := c.PathParamsBinder(p); err != nil {
-	// Validator Framework as a Workaround
 	if err := validator.New().Struct(p); err != nil {
 		log.Println("ERROR CONTROLLER:", err)
-		return c.JSON(http.StatusUnprocessableEntity, errorResponse{
-			Code:    http.StatusUnprocessableEntity,
-			Message: err.Error(),
-		})
+		return c.JSON(http.StatusUnprocessableEntity, err.Error())
 	}
 
 	fruits, err := fc.service.FilterFruits(p.Filter, p.Value)
-	// ERROR RESPONSE VALIDATIONS
+	// Error response validation
 	if err != nil {
-		// REPOSITORY ERRORS
 
-		// REPOSITORY FILE ERROR
-		if strings.HasSuffix(err.Error(), " no such file or directory") {
-			return c.JSON(http.StatusInternalServerError, &errorResponse{
-				Code:    http.StatusInternalServerError,
-				Message: err.Error(),
-			})
+		// Repository file error : internal server
+		if strings.HasSuffix(err.Error(), "no such file or directory") {
+			return c.JSON(http.StatusInternalServerError, err.Error())
 		}
 
-		// REPOSITORY PARSER ERROR
-		// NOTE: fruits with parser errors
-		// should return fruit valid fruit list, with default values and excluding invalid records?
-		// should i create a parser ERROR to type validation, inteast of string ?
+		// Repository parser error: partial content
+		// Return valid fruits and the parser error
 		if strings.HasPrefix(err.Error(), "parser error:") {
-			return c.JSON(http.StatusPartialContent, &getFruitsFilterResp{
-				Code:        http.StatusPartialContent,
+			return c.JSON(http.StatusPartialContent, &entity.FruitsFilterResponse{
 				Fruits:      fruits,
 				ParserError: err.Error(),
 			})
 		}
 
-		// DEFAULT RESPONSE
-		return c.JSON(http.StatusBadRequest, &errorResponse{
-			Code:    http.StatusBadRequest,
-			Message: err.Error(),
-		})
+		// Default response
+		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-	return c.JSON(http.StatusOK, &getFruitsFilterResp{
-		Code:   http.StatusOK,
-		Fruits: fruits,
-	})
+	return c.JSON(http.StatusOK, fruits)
 }
