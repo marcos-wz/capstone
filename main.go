4d99656d64065b03ddeec42d9fded0101a4f94ca
Marcos Jauregui <it.marcosj@gmail.com>
2022-02-03 13:16:27 -0600

    server implementation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/marcos-wz/capstone/internal/controller"
 	"github.com/marcos-wz/capstone/internal/repository"
 	"github.com/marcos-wz/capstone/internal/service"
@@ -10,20 +11,22 @@ import (
 func main() {
 
 	// Repo csv file path
-	// const csvFile string = "data/fruits.csv"
-	const csvFile string = "data/fruits-test-error.csv"
-	// NOTE: should i use environment variable,
-	// thinking in portable data for deployments
+	const csvFile string = "data/fruits.csv"
+	// const csvFile string = "data/fruits-test-error.csv"
+	// NOTE: should i use environment variable, USE VIPER
 	// var csvFile = os.Get("CSV_FILE")
 
 	e := echo.New()
+	e.Use(middleware.Logger())
 
 	// Get Fruits Filter handler
 	// NOTE: where should i put creators ?
-	// repo := repository.NewReaderRepo(csvFile)
-	// svc := service.NewFilterService(repo)
-	// ctrl := controller.NewFilterController(svc)
-	// e.GET("/v1/fruit/:filter/:value", ctrl.GetFruitsFilter)
+
+	repo := repository.NewReaderRepo(csvFile)
+	svc := service.NewFilterService(repo)
+	ctrl := controller.NewFilterController(svc)
+	e.GET("/v1/fruit/:filter/:value", ctrl.GetFruitsFilter)
+
 	e.GET("/v1/fruit/:filter/:value", func(c echo.Context) error {
 		repo := repository.NewReaderRepo(csvFile)
 		svc := service.NewFilterService(repo)
