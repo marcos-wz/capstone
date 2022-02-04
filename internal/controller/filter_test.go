e702033555cd3b0e96b5f51760c661ab78550bd1
Marcos Jauregui <it.marcosj@gmail.com>
2022-02-04 09:17:34 -0600

    FilterFruit unit test, all fruits test case

--- internal/controller/filter_test.go
+++ internal/controller/filter_test.go
@@ -7,54 +7,74 @@ import (
 	"testing"
 
 	"github.com/labstack/echo"
-	"github.com/marcos-wz/capstone/internal/repository"
-	"github.com/marcos-wz/capstone/internal/service"
+	"github.com/marcos-wz/capstone/internal/entity"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestGetFruitsFilter(t *testing.T) {
+// MOCK *******************************************
+
+type mockFilterService struct {
+	mock.Mock
+}
+
+func (mr *mockFilterService) GetFilteredFruits(filter *entity.FruitsFilterParams) ([]entity.Fruit, error) {
+	arg := mr.Called()
+	return arg.Get(0).([]entity.Fruit), arg.Error(1)
+}
+
+var mockFruitsData = []entity.Fruit{
+	{ID: 1, Name: "Naranja", Country: "Canada", Color: "Yellow"},
+	{ID: 2, Name: "manzana", Country: "Mexico", Color: "Red"},
+	{ID: 3, Name: "Pera", Country: "usa", Color: "Green"},
+	{ID: 4, Name: "Platano", Country: "USA", Color: "yellow"},
+}
+
+// UNIT TEST ***************************************
+
+func TestFilter_FilterFruit(t *testing.T) {
+	// Echo http server instance
+	e := echo.New()
+	// Test use cases
 	var testCases = []struct {
-		name        string
-		filterParam string
-		valueParam  string
-		respCode    int
-		respBody    string
+		name            string
+		params          *entity.FruitsFilterParams
+		serviceResponse []entity.Fruit
+		serviceError    error
+		responseCode    int
+		responseBody    string
 	}{
 		{
 			"Should return the fruits filtered, `Code: 200 - Fruits Filtered`",
-			"id",
-			"2",
+			&entity.FruitsFilterParams{Filter: "id", Value: "2"},
+			[]entity.Fruit{
+				{ID: 2, Name: "manzana", Country: "Mexico", Color: "Red"},
+			},
+			nil,
 			http.StatusOK,
 			`{"Code":400,"Message":`,
 		},
 	}
-	// NOTE: should i use echo or stdlib library ??
-	// Otherwise, this will be a layer depended of the ECHO http framework,
-	// But at the end of the day, this project use the ECHO http framework
-	e := echo.New()
-
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			// ************
-			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			// Request
+			url := "/v1/fruit/:filter/:value"
+			req := httptest.NewRequest(http.MethodGet, url, nil)
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
-			c.SetPath("/v1/fruit/:filter/:value")
 			c.SetParamNames("filter", "value")
-			c.SetParamValues(tc.filterParam, tc.valueParam)
-			// ************
-			// csvfile := "../../data/fruits-test-ok.csv"
-			// csvfile := "../../data/fruits-test-ok1.csv"
-			csvfile := "../../data/fruits-test-error.csv"
-			repo := repository.NewReaderRepo(csvfile)
-			svc := service.NewFilterService(repo)
-			h := NewFilterController(svc)
+			c.SetParamValues(tc.params.Filter, tc.params.Value)
+			// Mock
+			mock := mockFilterService{}
+			mock.On("GetFilteredFruits").Return(tc.serviceResponse, tc.serviceError)
+			// Controller
+			ctrl := NewFilterController(&mock)
 
 			// Assertions
-			if assert.NoError(t, h.GetFruitsFilter(c)) {
+			if assert.NoError(t, ctrl.FilterFruit(c)) {
 				t.Log("Resp CODE:", rec.Code)
 				t.Logf("Resp BODY: %s", rec.Body)
-				// assert.Equal(t, http.StatusOK, rec.Code)
+				assert.Equal(t, tc.responseCode, rec.Code)
 				// assert.Equal(t, userJSON, rec.Body.String())
 			}
 		})
