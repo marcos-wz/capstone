282f14e57365b141d8427dbd179870a3a9ce7f6e
Marcos Jauregui <it.marcosj@gmail.com>
2022-02-06 12:02:10 -0600

    define caducate days, and validation description comments

--- internal/entity/fruit.go
+++ internal/entity/fruit.go
@@ -3,14 +3,14 @@ package entity
 import "time"
 
 type Fruit struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Color       string  `json:"color"`
-	Unit        string  `json:"unit"`     // kgs, lbs
-	Price       float64 `json:"price"`    // price per unit
-	Stock       int     `json:"stock"`    // stock
-	Caducate    int     `json:"caducate"` // number days to the fruit be caducated
-	Country     string  `json:"country"`  // country imported from
-	CreatedAt   time.Time
+	ID           int       `json:"id" validate:"required"`            // Required, non-zero
+	Name         string    `json:"name" validate:"required,alphanum"` // Required, only letters and numbers allowed
+	Description  string    `json:"description"`                       // Optional
+	Color        string    `json:"color" validate:"required,alpha"`   // Required, only letters allowed
+	Unit         string    `json:"unit" validate:"oneof=kg lb"`       // Required, one of: kgs, lbs
+	Price        float64   `json:"price"`                             // Optional, the price per unit
+	Stock        int       `json:"stock"`                             // Optional, Items availability
+	CaducateDays int       `json:"caducate_days"`                     // Optional, the number days to spend the fruit to be caducated
+	Country      string    `json:"country" validate:"required,alpha"` // Rquired, the origin country only letters allowed
+	CreatedAt    time.Time `json:"created_at" validate:"required"`    // Required
 }
