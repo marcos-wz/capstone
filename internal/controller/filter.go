d5512080dec3481c96f8aa9472548d0a6335ca68
Marcos Jauregui <it.marcosj@gmail.com>
2022-02-08 09:00:38 -0600

    error propagation implementation

--- internal/controller/filter.go
+++ internal/controller/filter.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo"
@@ -18,7 +17,7 @@ type iFilterController interface {
 }
 
 type filterService interface {
-	GetFilteredFruits(filter *entity.FruitsFilterParams) ([]entity.Fruit, error)
+	GetFilteredFruits(filter *entity.FruitsFilterParams) ([]entity.Fruit, *entity.FruitsFilterError)
 }
 
 type filterController struct {
@@ -37,27 +36,41 @@ func (fc *filterController) FilterFruit(c echo.Context) error {
 		Filter: c.Param("filter"),
 		Value:  c.Param("value"),
 	}
+	// Entity Error response: Unprocessable Entity
 	if err := validator.New().Struct(filter); err != nil {
-		log.Println("ERROR CONTROLLER:", err)
-		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+		log.Println("ERROR Controller: entity validation - ", err)
+		return c.JSON(
+			http.StatusUnprocessableEntity,
+			map[string]string{"message": err.Error()},
+		)
 	}
 	fruits, err := fc.service.GetFilteredFruits(filter)
-	// Error response validation
+	// Error validations
 	if err != nil {
-		// Repository file error : internal server
-		if strings.HasSuffix(err.Error(), "no such file or directory") {
-			return c.JSON(http.StatusInternalServerError, err.Error())
+		switch err.Type {
+		// Repository File Error response: internal server error
+		case "Repo.FileError":
+			return c.JSON(
+				http.StatusInternalServerError,
+				map[string]string{"message": err.Error.Error()},
+			)
+		// Repository parser error response : partial content data with errors
+		case "Repo.ParserError":
+			return c.JSON(
+				http.StatusPartialContent,
+				map[string]interface{}{"fruits": fruits, "parser_errors": err.ParserErrors},
+			)
+		default:
+			// Default error response
+			return c.JSON(
+				http.StatusBadRequest,
+				map[string]string{"message": err.Error.Error()},
+			)
 		}
-		// Return valid fruits and the parser error
-		// Repository parser error: partial content (lost data)
-		if strings.HasPrefix(err.Error(), "parser error: ") {
-			return c.JSON(http.StatusPartialContent, &entity.FruitsFilterResponse{
-				Fruits:      fruits,
-				ParserError: err.Error(),
-			})
-		}
-		// Default response
-		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-	return c.JSON(http.StatusOK, fruits)
+	// Successful response
+	return c.JSON(
+		http.StatusOK,
+		map[string]interface{}{"fruits": fruits},
+	)
 }
