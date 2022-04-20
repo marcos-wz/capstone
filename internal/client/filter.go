b0ecb380af817827d280f68a14dd18bc2d30925d
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-20 16:57:59 -0500

    initial client filter

--- internal/client/filter.go
+++ internal/client/filter.go
@@ -2,24 +2,23 @@ package client
 
 import (
 	"context"
+	"github.com/marcos-wz/capstone/proto/filterpb"
 	"log"
 	"time"
-
-	pb "github.com/marcos-wz/capstone/internal/fruitpb"
 )
 
 func (c *client) Filter(filter, value string) {
 	log.Printf("Requesting Filter: %q, Value: %q ...", filter, value)
 	// Input Validation
 	filterAllowed := c.getAllowedFilter(filter)
-	if filterAllowed == pb.FiltersAllowed_UNDEFINED {
+	if filterAllowed == filterpb.FiltersAllowed_FILTER_UNDEFINED {
 		log.Printf("ERROR: filter %q undefined", filter)
 		return
 	}
 	// Service: contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.service.Filter(ctx, &pb.FilterRequest{
+	r, err := c.service.Filter(ctx, &filterpb.FilterRequest{
 		Filter: filterAllowed,
 		Value:  value,
 	})
@@ -27,6 +26,8 @@ func (c *client) Filter(filter, value string) {
 		log.Printf("ERROR: could not filter: %v", err)
 		return
 	}
-	log.Printf("Resp Code: %d", r.GetCode())
-	log.Printf("Resp Fruits: %s", r.GetFruits())
+
+	log.Printf("Resp Fruits: %s", r.GetFruit())
+	//log.Printf("Resp Code: %d", r.GetCode())
+	//log.Printf("Resp Fruits: %s", r.GetFruits())
 }
