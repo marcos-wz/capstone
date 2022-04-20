782d74faf5f79777758bacdc6802d42cd49cf705
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-20 16:59:10 -0500

    initial server filter and loader implementation

--- internal/server/filtercc.go
+++ internal/server/filtercc.go
@@ -3,8 +3,10 @@ package server
 import (
 	"context"
 	"github.com/marcos-wz/capstone/proto/filterccpb"
+	"log"
 )
 
 func (*server) FilterCC(ctx context.Context, in *filterccpb.FilterCCRequest) (*filterccpb.FilterCCResponse, error) {
+	log.Println("RPC Filter CC starting...")
 	return nil, nil
 }
--- internal/server/loader.go
+++ internal/server/loader.go
@@ -3,8 +3,10 @@ package server
 import (
 	"context"
 	"github.com/marcos-wz/capstone/proto/loaderpb"
+	"log"
 )
 
 func (*server) Loader(ctx context.Context, in *loaderpb.LoaderRequest) (*loaderpb.LoaderResponse, error) {
+	log.Println("RPC Loader starting...")
 	return nil, nil
 }
