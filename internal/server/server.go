569d424e59f05339f1b039da3d4f45e2fd7e7cda
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-20 19:16:08 -0500

    Server domain definition

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,24 +1,22 @@
 package server
 
 import (
+	"github.com/marcos-wz/capstone/internal/entity"
+	"github.com/marcos-wz/capstone/proto/basepb"
+	"github.com/marcos-wz/capstone/proto/filterpb"
 	"github.com/marcos-wz/capstone/proto/fruitpb"
 )
 
 type server struct {
-	// Services
-	//filterService   FilterService
-	//loaderService   LoaderService
-	//filterCCService FilterCCService
-
+	service fruitService
 	fruitpb.UnimplementedFruitServiceServer
 }
 
-// Buider server
-//func NewServer(filterSvc FilterService, loaderSvc LoaderService, filterCCSvc FilterCCService) *server {
-func NewServer() *server {
-	return &server{
-		//filterService:   filterSvc,
-		//loaderService:   loaderSvc,
-		//filterCCService: filterCCSvc,
-	}
+type fruitService interface {
+	GetFilteredFruits(filter *filterpb.FilterRequest) ([]*basepb.Fruit, *entity.FruitFilterError)
+}
+
+// NewServer returns a setup server
+func NewServer(svc fruitService) *server {
+	return &server{service: svc}
 }
