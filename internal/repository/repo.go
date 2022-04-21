b33a272ebc49515ea0845ed264aea49235dd0d28
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-20 19:15:28 -0500

    Fruit repository domain definition

--- internal/repository/repo.go
+++ internal/repository/repo.go
@@ -1,31 +1,33 @@
 package repository
 
-//
-//import (
-//	"github.com/marcos-wz/capstone/internal/entity"
-//	pb "github.com/marcos-wz/capstone/internal/fruit"
-//)
-//
-//// DOMAIN ***********************************************
-//
-//type iFruitRepo interface {
-//
-//	// ReadFruits fruits reader repository, read all fruit records from the
-//	// csv file, and guarantee csv data integrity.
-//	ReadFruits() ([]*pb.Fruit, *entity.ReadFruitsError)
-//
-//	// FetchFruits fetch data from external fruit
-//	FetchFruits() ([]*pb.Fruit, error)
-//
-//	// WriteFruits Write new fruits records to the csv file
-//	WriteFruits(fruits []*pb.Fruit) error
-//}
-//
-//type fruitRepo struct {
-//	filePath    string
-//	externalAPI string
-//}
-//
-//func NewFruitRepo() iFruitRepo {
-//	return &fruitRepo{}
-//}
+import (
+	"github.com/marcos-wz/capstone/internal/entity"
+	pb "github.com/marcos-wz/capstone/proto/basepb"
+)
+
+// DOMAIN ***********************************************
+
+type iFruitRepo interface {
+
+	// ReadFruits fruits reader repository, read all fruit records from the
+	// csv file, and guarantee csv data integrity.
+	ReadFruits() ([]*pb.Fruit, *entity.ReadFruitsError)
+
+	// FetchFruits fetch data from external fruit
+	FetchFruits() ([]*pb.Fruit, error)
+
+	// WriteFruits Write new fruits records to the csv file
+	WriteFruits(fruits []*pb.Fruit) error
+}
+
+type fruitRepo struct {
+	filePath    string
+	externalAPI string
+}
+
+func NewFruitRepo(file, url string) iFruitRepo {
+	return &fruitRepo{
+		filePath:    file,
+		externalAPI: url,
+	}
+}
