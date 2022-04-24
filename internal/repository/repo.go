85ae86d45b70fc58764b0a2dcf09e672a94e22e0
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-24 16:28:00 -0500

    change from custom error to standar error

--- internal/repository/repo.go
+++ internal/repository/repo.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"github.com/marcos-wz/capstone/internal/entity"
 	pb "github.com/marcos-wz/capstone/proto/basepb"
 )
 
@@ -14,7 +13,7 @@ type iFruitRepo interface {
 
 	// ReadFruits fruits reader repository, read all fruit records from the
 	// csv file, and guarantee csv data integrity.
-	ReadFruits() ([]*pb.Fruit, *entity.ReadFruitsError)
+	ReadFruits() ([]*pb.Fruit, error)
 
 	// FetchFruits fetch data from external fruit
 	FetchFruits() ([]*pb.Fruit, error)
