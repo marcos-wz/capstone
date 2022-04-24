66e538ddf3018514a8932d463588bcf9b8c218cf
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-23 20:36:11 -0500

    fruit parser csv record validations

--- internal/entity/fruit.go
+++ internal/entity/fruit.go
@@ -1,16 +1,17 @@
 package entity
 
-import "time"
-
-type Fruit struct {
-	ID           int       `json:"id" validate:"required"`            // Required, non-zero
-	Name         string    `json:"name" validate:"required,alphanum"` // Required, only letters and numbers allowed
-	Description  string    `json:"description"`                       // Optional
-	Color        string    `json:"color" validate:"required,alpha"`   // Required, only letters allowed
-	Unit         string    `json:"unit" validate:"oneof=kg lb"`       // Required, one of: kgs, lbs
-	Price        float64   `json:"price"`                             // Optional, the price per unit
-	Stock        int       `json:"stock"`                             // Optional, Items availability
-	CaducateDays int       `json:"caducate_days"`                     // Optional, the number days to spend the fruit to be caducated
-	Country      string    `json:"country" validate:"required,alpha"` // Rquired, the origin country only letters allowed
-	CreatedAt    time.Time `json:"created_at" validate:"required"`    // Required
+// FruitCSVRecord fruit with string values, and validation rules for csv parser
+type FruitCSVRecord struct {
+	Id           string `validate:"required,numeric,ne=0"`
+	Name         string `validate:"required,printascii,gt=2"`
+	Description  string `validate:"required,printascii,gt=2"`
+	Color        string `validate:"required,printascii,gt=2"`
+	Unit         string `validate:"oneof=KG LB"`
+	Price        string `validate:"required,numeric,ne=0.00,ne=0"`
+	Currency     string `validate:"oneof=MXN BRL CAD USD"`
+	Stock        string `validate:"required,numeric"`
+	CaducateDays string `validate:"required,numeric,ne=0"`
+	Country      string `validate:"oneof=MEXICO BRAZIL CANADA USA"`
+	CreateTime   string `validate:"required,numeric"`
+	UpdateTime   string `validate:"omitempty,numeric"`
 }
