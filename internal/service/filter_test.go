e203d7c78ac783d214ff33b998319d4cce1e7d70
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-25 00:24:47 -0500

    service get filtered fruits unit test implementation

--- internal/service/filter_test.go
+++ internal/service/filter_test.go
@@ -2,11 +2,11 @@ package service
 
 import (
 	"fmt"
-	"testing"
-
-	"github.com/marcos-wz/capstone/internal/entity"
 	"github.com/marcos-wz/capstone/internal/service/mocks"
+	"github.com/marcos-wz/capstone/proto/basepb"
+	"github.com/marcos-wz/capstone/proto/filterpb"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 // FilterFruits - UNIT TEST ****************************
@@ -14,133 +14,99 @@ import (
 func TestFilter_FilterFruits(t *testing.T) {
 
 	// Test data, just filters values
-	var mockFruitsData = []entity.Fruit{
-		{ID: 1, Name: "Naranja", Country: "Canada", Color: "Yellow"},
-		{ID: 2, Name: "manzana", Country: "Mexico", Color: "Red"},
-		{ID: 3, Name: "Pera", Country: "usa", Color: "Green"},
-		{ID: 4, Name: "Platano", Country: "USA", Color: "yellow"},
+	var mockFruitsData = []*basepb.Fruit{
+		{Id: 1, Name: "Naranja", Country: basepb.Country_COUNTRY_CANADA, Color: "Yellow"},
+		{Id: 2, Name: "manzana", Country: basepb.Country_COUNTRY_MEXICO, Color: "Red"},
+		{Id: 3, Name: "Pera", Country: basepb.Country_COUNTRY_USA, Color: "Green"},
+		{Id: 4, Name: "Platano", Country: basepb.Country_COUNTRY_USA, Color: "yellow"},
 	}
-
 	// test use cases
 	var testCases = []struct {
 		name         string
-		filter       *entity.FruitsFilterParams
-		repoResponse []entity.Fruit
-		repoErr      *entity.ReadFruitsError
-		response     []entity.Fruit
-		err          *entity.FruitFilterError
+		filter       *filterpb.FilterRequest
+		repoResponse []*basepb.Fruit
+		repoErr      error
+		response     []*basepb.Fruit
+		err          error
 	}{
 		{
 			"Should return the fruit filtered by ID 1, no errors",
-			&entity.FruitsFilterParams{Filter: "id", Value: "1"},
-			mockFruitsData,
-			nil,
-			[]entity.Fruit{{ID: 1, Name: "Naranja", Country: "Canada", Color: "Yellow"}},
-			nil,
-		},
-		{
-			"Should return error, Invalid Filter ID: invalid syntax",
-			&entity.FruitsFilterParams{Filter: "id", Value: "badvalue"},
-			mockFruitsData,
-			nil,
-			nil,
-			&entity.FruitFilterError{
-				Type:  "Service.FilterError",
-				Error: fmt.Errorf("invalid ID filter(badvalue): strconv.Atoi: parsing \"badvalue\": invalid syntax"),
+			&filterpb.FilterRequest{
+				Filter: filterpb.FiltersAllowed_FILTER_ID,
+				Value:  "1",
 			},
+			mockFruitsData,
+			nil,
+			[]*basepb.Fruit{
+				{Id: 1, Name: "Naranja", Country: basepb.Country_COUNTRY_CANADA, Color: "Yellow"},
+			},
+			nil,
 		},
 		{
 			"Should return the fruit filtered by NAME, no errors",
-			&entity.FruitsFilterParams{Filter: "name", Value: "pera"},
+			&filterpb.FilterRequest{Filter: filterpb.FiltersAllowed_FILTER_NAME, Value: "pera"},
 			mockFruitsData,
 			nil,
-			[]entity.Fruit{{ID: 3, Name: "Pera", Country: "usa", Color: "Green"}},
+			[]*basepb.Fruit{
+				{Id: 3, Name: "Pera", Country: basepb.Country_COUNTRY_USA, Color: "Green"},
+			},
 			nil,
 		},
 		{
 			"Should return the fruit filtered by COLOR, no errors",
-			&entity.FruitsFilterParams{Filter: "color", Value: "yellow"},
+			&filterpb.FilterRequest{Filter: filterpb.FiltersAllowed_FILTER_COLOR, Value: "yellow"},
 			mockFruitsData,
 			nil,
-			[]entity.Fruit{
-				{ID: 1, Name: "Naranja", Country: "Canada", Color: "Yellow"},
-				{ID: 4, Name: "Platano", Country: "USA", Color: "yellow"},
+			[]*basepb.Fruit{
+				{Id: 1, Name: "Naranja", Country: basepb.Country_COUNTRY_CANADA, Color: "Yellow"},
+				{Id: 4, Name: "Platano", Country: basepb.Country_COUNTRY_USA, Color: "yellow"},
 			},
 			nil,
 		},
 		{
 			"Should return the fruit filtered by COUNTRY, no errors",
-			&entity.FruitsFilterParams{Filter: "country", Value: "USA"},
+			&filterpb.FilterRequest{Filter: filterpb.FiltersAllowed_FILTER_COUNTRY, Value: "usa"},
 			mockFruitsData,
 			nil,
-			[]entity.Fruit{
-				{ID: 3, Name: "Pera", Country: "usa", Color: "Green"},
-				{ID: 4, Name: "Platano", Country: "USA", Color: "yellow"},
+			[]*basepb.Fruit{
+				{Id: 3, Name: "Pera", Country: basepb.Country_COUNTRY_USA, Color: "Green"},
+				{Id: 4, Name: "Platano", Country: basepb.Country_COUNTRY_USA, Color: "yellow"},
 			},
 			nil,
-		},
-		{
-			"Should return error, Invalid Filter: undefined filter",
-			&entity.FruitsFilterParams{Filter: "badfilter", Value: "badvalue"},
-			mockFruitsData,
-			nil,
-			nil,
-			&entity.FruitFilterError{
-				Type:  "Service.FilterError",
-				Error: fmt.Errorf("undefined filter(badfilter): badvalue"),
-			},
 		},
 		// Repository error: no such file
 		{
 			"Should return repository error, open : no such file or directory",
-			&entity.FruitsFilterParams{Filter: "id", Value: "1"},
+			&filterpb.FilterRequest{Filter: filterpb.FiltersAllowed_FILTER_ID, Value: "1"},
 			nil,
-			&entity.ReadFruitsError{
-				Type:  "Repo.FileError",
-				Error: fmt.Errorf("open : no such file or directory"),
-			},
+			fmt.Errorf("open : no such file or directory"),
 			nil,
-			&entity.FruitFilterError{
-				Type:  "Repo.FileError",
-				Error: fmt.Errorf("open : no such file or directory"),
-			},
-		},
-		// Repository error: parser error
-		{
-			"Should return repository error, parser error:",
-			&entity.FruitsFilterParams{Filter: "id", Value: "1"},
-			mockFruitsData,
-			&entity.ReadFruitsError{
-				Type:  "Repo.ParserError",
-				Error: fmt.Errorf("reader repository, parse fruit errors found"),
-			},
-			[]entity.Fruit{
-				{ID: 1, Name: "Naranja", Country: "Canada", Color: "Yellow"},
-			},
-			&entity.FruitFilterError{
-				Type:  "Repo.ParserError",
-				Error: fmt.Errorf("reader repository, parse fruit errors found"),
-			},
+			fmt.Errorf("open : no such file or directory"),
 		},
 	}
 
-	// *************************************
-
+	// RUN TESTS --------------------------------------
+	//Debug = true
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			// MOCK
-			mock := &mocks.ReaderRepo{}
+			mock := mocks.NewFruitRepo(t)
 			mock.On("ReadFruits").Return(tc.repoResponse, tc.repoErr)
-			// SERVICE
-			service := NewFilterService(mock)
+			//// SERVICE
+			service := NewFruitService(mock)
 			fruits, err := service.GetFilteredFruits(tc.filter)
-			assert.Equal(t, tc.err, err)
-			assert.Equal(t, tc.response, fruits)
-			t.Log("Total fruits:", len(fruits))
-			for _, f := range fruits {
-				t.Logf("%+v", f)
+			if Debug {
+				t.Log("Total fruits:", len(fruits))
+				t.Logf("Err: %v", err)
+				for _, f := range fruits {
+					t.Logf("%+v", f)
+				}
 			}
-
+			assert.Equal(t, tc.err, err)
+			assert.Len(t, tc.response, len(fruits))
+			for i := 0; i < len(fruits); i++ {
+				assert.EqualValues(t, tc.response[i], fruits[i])
+			}
 		})
 
 	}
