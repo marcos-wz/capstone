67c8b264c9d62d36befb95e4e388882a169c29e4
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-25 00:20:57 -0500

    parse currency unit test implementation

---
+++ internal/parser/currency.go
@@ -0,0 +1,22 @@
+package parser
+
+import (
+	"github.com/marcos-wz/capstone/proto/basepb"
+	"log"
+)
+
+func (*fruitParser) ParseCurrency(currency string) basepb.Currency {
+	switch currency {
+	case "MXN":
+		return basepb.Currency_CURRENCY_MXN
+	case "BRL":
+		return basepb.Currency_CURRENCY_BRL
+	case "CAD":
+		return basepb.Currency_CURRENCY_CAD
+	case "USD":
+		return basepb.Currency_CURRENCY_USD
+	default:
+		log.Printf("REPO-WARNING: currency parser : currency %q undefined", currency)
+		return basepb.Currency_CURRENCY_UNDEFINED
+	}
+}
