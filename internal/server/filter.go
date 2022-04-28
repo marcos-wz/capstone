c2d991c04e7c037b547dd1228905c22d3fa15170
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-28 06:39:08 -0500

    filter fruits server implementation

--- internal/server/filter.go
+++ internal/server/filter.go
@@ -1,9 +1,9 @@
 package server
 
 import (
-	"context"
-	"github.com/marcos-wz/capstone/proto/basepb"
 	"github.com/marcos-wz/capstone/proto/filterpb"
+	"github.com/marcos-wz/capstone/proto/fruitpb"
+	"io/fs"
 	"log"
 )
 
@@ -13,111 +13,55 @@ import (
 //	- value: the filter value request
 // RESPONSES:
 // 	- 200 Status OK: returns filter response with fruits filtered list
-//	- 206 Partial Content: returns fruits filtered  partial list and reader parser errors(invalid csv file data!!)
 //	- 422 Unprocessable Entity : returns param filter and value errors
 //	- 500 Internal Server : returns reader CSV File error (critical!)
 //	- 400 Bad Request: default errors
-func (s *server) Filter(ctx context.Context, filterReq *filterpb.FilterRequest) (*filterpb.FilterResponse, error) {
+func (s *server) Filter(filterReq *filterpb.FilterRequest, stream fruitpb.FruitService_FilterServer) error {
 	if Debug {
 		log.Println("RPC Filter: starting...")
 		log.Printf("RPC Filter: filter request: %+v", filterReq)
 	}
 	// SERVICE
-	s.service.GetFilteredFruits(filterReq)
-	//fruits, err := s.service.GetFilteredFruits(filterReq)
+	fruits, err := s.service.GetFilteredFruits(filterReq)
 	// Error validations
-	//if err != nil {
-	//	switch err.Type {
-	//	// Repository File Error response: internal server error
-	//	case "Repo.FileError":
-	//		return c.JSON(http.StatusInternalServerError, &entity.ErrorResponse{
-	//			Message: err.Error.Error(),
-	//		})
-	//	// Repository parser error response : partial fruits with parser errors
-	//	case "Repo.ParserError":
-	//		return c.JSON(http.StatusPartialContent, &entity.FruitFilterResponse{
-	//			Fruits:       fruits,
-	//			ParserErrors: err.ParserErrors,
-	//		})
-	//	default:
-	//		// Default error response
-	//		return c.JSON(http.StatusBadRequest, &entity.ErrorResponse{
-	//			Message: err.Error.Error(),
-	//		})
-	//	}
-	//}
-	// Successful response
+	if err != nil {
+		log.Printf("Error: %T - %v", err, err)
 
-	// *************************************
-	return &filterpb.FilterResponse{
-		Fruit: &basepb.Fruit{
-			Name: "fruit test",
-		},
-	}, nil
+		switch err.(type) {
+		case *fs.PathError:
+			log.Printf("REPO ERROR: %v", err)
+			return err
+		default:
+			return err
+		}
+
+		//switch err.Type {
+		//// Repository File Error response: internal server error
+		//case "Repo.FileError":
+		//	return c.JSON(http.StatusInternalServerError, &entity.ErrorResponse{
+		//		Message: err.Error.Error(),
+		//	})
+		//// Repository parser error response : partial fruits with parser errors
+		//case "Repo.ParserError":
+		//	return c.JSON(http.StatusPartialContent, &entity.FruitFilterResponse{
+		//		Fruits:       fruits,
+		//		ParserErrors: err.ParserErrors,
+		//	})
+		//default:
+		//	// Default error response
+		//	return c.JSON(http.StatusBadRequest, &entity.ErrorResponse{
+		//		Message: err.Error.Error(),
+		//	})
+		//}
+	}
+	// Response
+	log.Printf("Fruits found: %d \n%v", len(fruits), fruits)
+	for _, fruit := range fruits {
+		res := &filterpb.FilterResponse{Fruit: fruit}
+		err := stream.Send(res)
+		if err != nil {
+			log.Printf("ERROR: sending stream: %v", err)
+		}
+	}
+	return nil
 }
-
-//func (s *server) Filter(ctx context.Context, req *pb.FilterRequest) (*pb.FilterResponse, error) {
-
-//	// Filter request validation. Invalid filter response unprocessable entity
-//	filter := &entity.FruitsFilterParams{
-//		Filter: req.GetFilter().String(),
-//		Value:  req.GetValue(),
-//	}
-//
-//	if err := validator.New().Struct(filter); err != nil {
-//		log.Println("ERROR: filter request validation - ", err)
-//		return &pb.FilterResponse{
-//			Code:  422,
-//			Error: err.Error(),
-//		}, fmt.Errorf("unprocessable entity: %v", err)
-//	}
-//
-//	// Get Fruits from service
-//	fruits, err := s.filterService.GetFilteredFruits(filter)
-//	// Error validations
-//	if err != nil {
-//		switch err.Type {
-//		// Repository File Error response: internal server error
-//		case "Repo.FileError":
-//			return &pb.FilterResponse{
-//				Code:  500,
-//				Error: err.Error.Error(),
-//			}, fmt.Errorf("repository file error: %v", err.Error)
-//		// Repository parser error response : partial fruits with parser errors
-//		case "Repo.ParserError":
-//			// Parser records
-//			// for _, e := range err.ParserErrors {
-//			// 	log.Printlnf(.Record)
-//			// }
-//			return &pb.FilterResponse{
-//				Code:  206,
-//				Error: err.Error.Error(),
-//			}, fmt.Errorf("repository parser error: %v", err.Error)
-//		default:
-//			return &pb.FilterResponse{
-//				Code:  404,
-//				Error: err.Error.Error(),
-//			}, fmt.Errorf("bad request: %v", err.Error)
-//		}
-//	}
-//
-//	// Mapping fruits from to Protocol Buffers
-//	// log.Println("Fruits: ", fruits)
-//	fruitsPb := []*pb.Fruit{}
-//	for _, f := range fruits {
-//		fruitsPb = append(fruitsPb, &pb.Fruit{
-//			Id:          uint32(f.ID),
-//			Name:        f.Name,
-//			Description: f.Description,
-//			Color:       f.Color,
-//			Price:       float32(f.Price),
-//			Stock:       uint32(f.Stock),
-//			Country:     f.Country,
-//		})
-//	}
-//	resp := &pb.FilterResponse{
-//		Code:   200,
-//		Fruits: fruitsPb,
-//	}
-//	return resp, nil
-//}
