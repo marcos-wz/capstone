39eb7be332aa448830051efc205b033d087d10fe
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-28 06:41:48 -0500

    manage server configuration from json

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,38 +11,32 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/marcos-wz/capstone/internal/server"
-	"github.com/spf13/viper"
 )
 
 func main() {
-
-	// Config
-	viper.SetConfigName("server")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./configs/")
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("fatal error config file: server \n %v", err)
+	// Load Config
+	serverConfig, err := server.LoadServerConfig("./config/server.json")
+	if err != nil {
+		log.Fatalf("FATAL: %v", err)
 	}
 
 	// Fruit repository
-	repo := repository.NewFruitRepo(viper.GetString("data.csv"), viper.GetString("external-api.fruits"))
+	repo := repository.NewFruitRepo(serverConfig.CSVFile, serverConfig.ExternalAPI)
 	// Fruit service
 	svc := service.NewFruitService(repo)
-	// Server builder
-	srv := server.NewServer(svc)
+	// Fruit server
+	fruitServer := server.NewFruitServer(svc)
 
 	// Listener
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", viper.GetInt("server.port")))
+	lis, err := net.Listen("tcp", fmt.Sprintf("%v:%d", serverConfig.Host, serverConfig.Port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
-	// Server
+	// GRPC server
 	s := grpc.NewServer()
-	fruitpb.RegisterFruitServiceServer(s, srv)
+	fruitpb.RegisterFruitServiceServer(s, fruitServer)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-
 }
