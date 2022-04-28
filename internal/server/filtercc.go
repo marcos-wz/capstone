e894e83e24fa45451fa847f9685aa1f35c1ea60f
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-28 06:34:05 -0500

    migrate from unary to stream service

--- internal/server/filtercc.go
+++ internal/server/filtercc.go
@@ -1,12 +1,12 @@
 package server
 
 import (
-	"context"
 	"github.com/marcos-wz/capstone/proto/filterccpb"
+	"github.com/marcos-wz/capstone/proto/fruitpb"
 	"log"
 )
 
-func (*server) FilterCC(ctx context.Context, in *filterccpb.FilterCCRequest) (*filterccpb.FilterCCResponse, error) {
+func (*server) FilterCC(req *filterccpb.FilterCCRequest, stream fruitpb.FruitService_FilterCCServer) error {
 	log.Println("RPC Filter CC starting...")
-	return nil, nil
+	return nil
 }
