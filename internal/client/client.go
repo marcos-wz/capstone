68ee12b53ee8aac05e737885d88411ec5ee11c1c
Marcos Jauregui <mjaureguisolis@octolabs.io>
2022-04-28 06:26:36 -0500

    client contract definition

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,42 +1,22 @@
 package client
 
 import (
-	"github.com/marcos-wz/capstone/proto/filterpb"
 	"github.com/marcos-wz/capstone/proto/fruitpb"
-	"strings"
-
 	"google.golang.org/grpc"
 )
 
-// CONTRACT
-type iClient interface {
+type FruitClient interface {
 	Filter(filter, value string)
 	Loader()
 	FilterCC(filter, value string)
 }
 
-type client struct {
+type fruitClient struct {
 	service fruitpb.FruitServiceClient
 }
 
-func NewClient(conn *grpc.ClientConn) iClient {
-	return &client{
+func NewFruitClient(conn *grpc.ClientConn) FruitClient {
+	return &fruitClient{
 		service: fruitpb.NewFruitServiceClient(conn),
-	}
-}
-
-// IMPLEMENTATION
-func (*client) getAllowedFilter(value string) filterpb.FiltersAllowed {
-	switch strings.ToUpper(value) {
-	case "ID":
-		return filterpb.FiltersAllowed_FILTER_ID
-	case "NAME":
-		return filterpb.FiltersAllowed_FILTER_NAME
-	case "COLOR":
-		return filterpb.FiltersAllowed_FILTER_COLOR
-	case "COUNTRY":
-		return filterpb.FiltersAllowed_FILTER_COUNTRY
-	default:
-		return filterpb.FiltersAllowed_FILTER_UNDEFINED
 	}
 }
