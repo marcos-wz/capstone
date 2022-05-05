8797767123fc3a24494e20e9715e42c82a881aa7
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-04 19:01:11 -0500

    relocate load config server to server package

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,4 +1,4 @@
-package client
+package server
 
 import (
 	"encoding/json"
@@ -8,24 +8,23 @@ import (
 	"log"
 )
 
-// LoadClientConfig reads configuration from file and validates the values
-func LoadClientConfig(file string) (*entity.ClientConfig, error) {
+// LoadServerConfig reads configuration from file and validates the values
+func LoadServerConfig(file string) (*entity.ServerConfig, error) {
 	// File
 	f, err := ioutil.ReadFile(file)
 	if err != nil {
-		log.Printf("CLIENT-ERROR: reading config file: %q : %v", file, err)
+		log.Printf("SERVER-ERROR: reading config file: %q : %v", file, err)
 		return nil, err
 	}
 	// Decode
-	config := &entity.ClientConfig{}
+	config := &entity.ServerConfig{}
 	if err := json.Unmarshal(f, config); err != nil {
-		log.Printf("CLIENT-ERROR: decoding config file: %q : %v", file, err)
+		log.Printf("SERVER-ERROR: decoding config file: %q : %v", file, err)
 		return nil, err
 	}
-
 	// Validate Input data
 	if err := validator.New().Struct(config); err != nil {
-		log.Println("CLIENT-ERROR: input validator: ")
+		log.Println("SERVER-ERROR: input validator: ")
 		for _, e := range err.(validator.ValidationErrors) {
 			log.Printf("Field: %v, Value: %v, Tag: %v, Param: %v", e.StructField(), e.Value(), e.Tag(), e.Param())
 		}
