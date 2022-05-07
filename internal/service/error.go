f8a34871186b97ea40a51ab05964fb8ffc08d01b
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-07 12:31:31 -0500

    service error management

--- internal/service/error.go
+++ internal/service/error.go
@@ -7,10 +7,14 @@ const (
 	ErrUndefined entity.ErrorType = 0
 
 	// ErrFilterFactory general factory error
-	ErrFilterFactory     entity.ErrorType = 20
-	ErrFilterFactoryDesc string           = "service filter factory error"
+	ErrFilterFactory entity.ErrorType = 20
 
 	// ErrFilterFactoryID error factory on the ID
-	ErrFilterFactoryID     entity.ErrorType = 21
-	ErrFilterFactoryIDDesc string           = "service filter factory ID error"
+	ErrFilterFactoryID entity.ErrorType = 21
 )
+
+var ErrDesc = map[entity.ErrorType]string{
+	ErrUndefined:       "undefined error",
+	ErrFilterFactory:   "service filter factory error",
+	ErrFilterFactoryID: "service filter factory ID error",
+}
