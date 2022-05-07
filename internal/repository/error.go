5d864409c205f5d68adf14cc9779b4c06cccf062
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-07 12:31:08 -0500

    repository error management

--- internal/repository/error.go
+++ internal/repository/error.go
@@ -4,18 +4,26 @@ import "github.com/marcos-wz/capstone/internal/entity"
 
 //	Error range: 10 - 19 repository errors
 const (
-	ErrUndefined     entity.ErrorType = 0
-	errUndefinedDesc string           = "undefined error"
+	ErrUndefined entity.ErrorType = 0
 
 	// ErrFileCSV file error on csv files
-	ErrFileCSV     entity.ErrorType = 10
-	errFileCSVDesc string           = "csv file error"
+	ErrFileCSV entity.ErrorType = 10
 
 	// ErrFileJSON file error on json files
-	ErrFileJSON     entity.ErrorType = 11
-	errFileJSONDesc string           = "json file error"
+	ErrFileJSON entity.ErrorType = 11
 
 	// ErrParseFruitCSV parse fruit from csv records error
-	ErrParseFruitCSV     entity.ErrorType = 12
-	errParseFruitCSVDesc string           = "parse fruit error"
+	ErrParseFruitCSV           entity.ErrorType = 12
+	ErrParseFruitCSVIndex      entity.ErrorType = 13
+	ErrParseFruitCSVValidation entity.ErrorType = 14
 )
+
+// ErrDesc holds repository errors description
+var ErrDesc = map[entity.ErrorType]string{
+	ErrUndefined:               "undefined error",
+	ErrFileCSV:                 "csv file error",
+	ErrFileJSON:                "json file error",
+	ErrParseFruitCSV:           "parse fruit error",
+	ErrParseFruitCSVIndex:      "parse fruit csv record undefined index error",
+	ErrParseFruitCSVValidation: "parse fruit csv record validation error",
+}
