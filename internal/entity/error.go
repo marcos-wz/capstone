34f02c16c64ca6f204b8dbe7a3e62fcb33969530
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-07 12:30:49 -0500

    set Err type error in fruit struct

--- internal/entity/error.go
+++ internal/entity/error.go
@@ -2,15 +2,19 @@ package entity
 
 import "fmt"
 
+//	Error range: 10 - 19 repository errors
+
+// ErrorType holds the error type
 type ErrorType uint32
 
+// FruitError system default error definition
 type FruitError struct {
-	Type     ErrorType
-	TypeDesc string
-	Message  string
+	Type ErrorType
+	Desc string
+	Err  error
 }
 
 // Error contract method for Error interface, returns type description and message,
 func (fe *FruitError) Error() string {
-	return fmt.Sprintf("%v: %v", fe.TypeDesc, fe.Message)
+	return fmt.Sprintf("%v: %v", fe.Desc, fe.Err)
 }
