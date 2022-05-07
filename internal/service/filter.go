2e5080b597cbbd7fe31bb09eee05c0d5ea30319e
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-07 12:37:00 -0500

    merge server implementation to service layer

--- internal/service/filter.go
+++ internal/service/filter.go
@@ -1,20 +1,42 @@
 package service
 
 import (
-	pb "github.com/marcos-wz/capstone/proto/basepb"
+	"fmt"
 	"github.com/marcos-wz/capstone/proto/filterpb"
+	"github.com/marcos-wz/capstone/proto/fruitpb"
 	"log"
 )
 
-func (f *fruitService) FilterFruits(filter *filterpb.FilterRequest) ([]*pb.Fruit, error) {
-	if Debug {
-		log.Println("SVC: GetFilteredFruits: starting...")
+func (fs *fruitService) Filter(req *filterpb.FilterRequest, stream fruitpb.FruitService_FilterServer) error {
+	if DebugLevel >= 1 {
+		log.Println("RPC Filter starting...")
+		log.Printf("RPC Filter request: %v", req)
 	}
 	// Repository
-	fruits, err := f.repo.ReadFruits()
+	fruits, err := fs.repo.ReadFruits()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	// Filter Fruit List
-	return filterFactory(fruits, filter)
+	fruits, err = filterFactory(fruits, req)
+	if err != nil {
+		return err
+	}
+	// Response
+	if DebugLevel >= 1 {
+		log.Printf("Filtered fruits found: %d", len(fruits))
+	}
+	for _, fruit := range fruits {
+		if DebugLevel >= 1 {
+			log.Printf("Sending fruit: %v ...", fruit)
+		}
+		res := &filterpb.FilterResponse{Fruit: fruit}
+		err := stream.Send(res)
+		if err != nil {
+			errStream := fmt.Errorf("sending stream: %v", err)
+			log.Printf("ERROR: %v", errStream)
+			return err
+		}
+	}
+	return nil
 }
