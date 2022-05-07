10df41651283701ebf1adb56df69b304794ff7c3
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-07 12:43:21 -0500

    load server config from package configs, and remove server package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/marcos-wz/capstone/internal/app"
-	"github.com/marcos-wz/capstone/internal/repository"
-	"github.com/marcos-wz/capstone/internal/service"
-	"github.com/marcos-wz/capstone/proto/fruitpb"
+	config "github.com/marcos-wz/capstone/configs"
+
 	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 
-	"github.com/marcos-wz/capstone/internal/server"
+	"github.com/marcos-wz/capstone/internal/repository"
+	"github.com/marcos-wz/capstone/internal/service"
+	"github.com/marcos-wz/capstone/proto/fruitpb"
 )
 
 func main() {
 
 	// Load Config
-	serverConfig, err := app.LoadServerConfig("./config/server.json")
+	serverConfig, err := config.LoadServerConfig("./configs/server.json")
 	if err != nil {
 		log.Fatalf("FATAL: %v", err)
 	}
@@ -29,10 +29,8 @@ func main() {
 	}
 	// Fruit repository
 	repo := repository.NewFruitRepo(serverConfig.CSVFile, serverConfig.ExternalAPI)
-	// Fruit service
-	svc := service.NewFruitService(repo)
-	// Fruit server
-	fruitServer := server.NewFruitServer(svc)
+	// Fruit services
+	services := service.NewFruitService(repo)
 
 	// Listener
 	lis, err := net.Listen("tcp", fmt.Sprintf("%v:%d", serverConfig.Host, serverConfig.Port))
@@ -41,8 +39,8 @@ func main() {
 	}
 	// GRPC server
 	s := grpc.NewServer(grpc.Creds(creds))
-	fruitpb.RegisterFruitServiceServer(s, fruitServer)
-	log.Printf("server listening at %v", lis.Addr())
+	fruitpb.RegisterFruitServiceServer(s, services)
+	log.Printf("Server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
