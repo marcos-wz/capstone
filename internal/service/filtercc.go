b88f36e1142706ea711848a0a4a9df50abc2dd6d
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-07 12:39:26 -0500

    merge server implementation to service layer

--- internal/service/filtercc.go
+++ internal/service/filtercc.go
@@ -1,4 +1,4 @@
-package server
+package service
 
 import (
 	"github.com/marcos-wz/capstone/proto/filterccpb"
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-func (*server) FilterCC(req *filterccpb.FilterCCRequest, stream fruitpb.FruitService_FilterCCServer) error {
+func (*fruitService) FilterCC(req *filterccpb.FilterCCRequest, stream fruitpb.FruitService_FilterCCServer) error {
 	log.Println("RPC Filter CC starting...")
 	log.Printf("RPC Filter CC request: %v", req)
 	log.Printf("RPC Filter CC stream: %v", stream)
--- internal/service/loader.go
+++ internal/service/loader.go
@@ -1,6 +1,17 @@
 package service
 
-// DOMAIN ***********************************************
+import (
+	"context"
+	"github.com/marcos-wz/capstone/proto/loaderpb"
+	"log"
+)
+
+func (*fruitService) Loader(ctx context.Context, req *loaderpb.LoaderRequest) (*loaderpb.LoaderResponse, error) {
+	log.Println("RPC Loader starting...")
+	log.Printf("RPC Loader request: %v", req)
+	log.Printf("RPC Loader context: %v", ctx)
+	return nil, nil
+}
 
 //type iLoaderService interface {
 //	// Add new fruits from external API in the repository, and returns total fruit inserted
