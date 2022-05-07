8087ac4ff7aba2397ef0b4a6ca513c961568b676
Marcos Jauregui <marcos.jauregui@wizeline.com>
2022-05-07 12:22:47 -0500

    move client config struct to config.go

--- internal/entity/client.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-// ClientConfig entity configuration file with validation rules
-type ClientConfig struct {
-	ServerHost string `json:"server-host" validate:"omitempty,hostname"`
-	ServerPort int    `json:"server-port" validate:"required,min=1"`
-	Debug      bool   `json:"debug"`
-	SSLCert    string `json:"ssl-cert" validate:"file"`
-}
